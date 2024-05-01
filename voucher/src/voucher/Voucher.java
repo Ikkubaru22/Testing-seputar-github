426d0434673214aacd7693823df54213256328ad
Ikkubaru22 <150113094+Ikkubaru22@users.noreply.github.com>
2024-05-02 04:17:51 +0700

    first commit

---
+++ voucher/src/voucher/Voucher.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package voucher;
+
+/**
+ *
+ * @author ikkub
+ */
+public class Voucher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Percobaan Voucher");
+        
+        System.out.println("Bulat kanan");
+        System.out.println("Bulat kanan");
+        System.out.println("Kiri kotak");
+        System.out.println("Segitiga Atas");
+    }
+    
+}
